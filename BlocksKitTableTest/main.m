8a405fdf220555803b26715aa31712ea8fb2534a
Alex Gray <alex@mrgray.com>
2013-07-04 12:19:30 -0400

    Sample for reference for issue of non-functional A2DynamicDelegate component of BlocksKit.

--- BlocksKitTableTest/main.m
+++ BlocksKitTableTest/main.m
@@ -1,14 +1,29 @@
-//
-//  main.m
-//  BlocksKitTableTest
-//
-//  Created by Alex Gray on 7/4/13.
-//  Copyright (c) 2013 Alex Gray. All rights reserved.
-//
 
-#import <Cocoa/Cocoa.h>
+#import <BlocksKit/BlocksKit.h>
 
-int main(int argc, const char * argv[])
-{
-	return NSApplicationMain(argc, argv);
-}
+@interface 							AppDelegate : NSObject
+@property (assign) IBOutlet 	NSTableView * tv;
+@property (strong) 		A2DynamicDelegate * ddSource;						@end
+
+@implementation AppDelegate
+
+- (void)awakeFromNib 	{
+
+	_ddSource	 	= _tv.dynamicDataSource;									logObject(_ddSource);
+	_tv.dataSource	= (id<NSTableViewDataSource>)_ddSource;				logObject(_tv.dataSource);
+
+	SEL rowSEL = @selector					 (numberOfRowsInTableView:);
+	SEL objSEL = @selector(tableView:objectValueForTableColumn:row:);
+
+	NSInteger(^reqIMP1)(NSTableView*) =
+  ^NSInteger			 (NSTableView*v)	{ return 9; };
+
+	 id(^reqIMP2)(NSTableView*, NSTableColumn*, NSInteger) =
+   ^id 			 (NSTableView*v,NSTableColumn*c,NSInteger r) { return @"DOESNT WORK!"; };
+
+	[_ddSource implementMethod:rowSEL withBlock:reqIMP1];
+	[_ddSource implementMethod:objSEL withBlock:reqIMP2];
+	[_tv reloadData];
+}																								@end
+
+int main(int argc, const char * argv[])	{	return NSApplicationMain(argc, argv);	}
